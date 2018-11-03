7f0dda548256def3f7c6d9a86cfb6eab43d3540e
your name <your email>
2018-11-03 15:38:03 +0800

    version update ccs

--- web1/settings.py
+++ web1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k)3t)&81j1yk)#hmb29783^=qx0^^etdai1wo=6o&7i%jiz42e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.159.200.79']
+ALLOWED_HOSTS = []
 
 
 # Application definition
